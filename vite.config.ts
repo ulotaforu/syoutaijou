da67f72fb82691e63a83160bd8f5a0e40e5fa817
ulotaforu <ulotaforu@gmail.com>
2025-06-28 18:01:15 +0900

    フォーマット

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import build from '@hono/vite-build/cloudflare-workers'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import tailwindcss from '@tailwindcss/vite'
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import build from "@hono/vite-build/cloudflare-workers";
+import adapter from "@hono/vite-dev-server/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ['./app/style.css'] }
-    }),
-    tailwindcss(),
-    build()
-  ],
-  server: {
-    host: "0.0.0.0",
-    port: 5173
-  }
-})
+	plugins: [
+		honox({
+			devServer: { adapter },
+			client: { input: ["./app/style.css"] },
+		}),
+		tailwindcss(),
+		build(),
+	],
+	server: {
+		host: "0.0.0.0",
+		port: 5173,
+	},
+});
