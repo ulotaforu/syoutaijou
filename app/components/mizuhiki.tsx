a0e1cc1ca7575e5a46e202ad15984d8d9f68cde7
ulotaforu <ulotaforu@gmail.com>
2025-08-07 00:06:11 +0900

    スマホ対応

--- app/components/mizuhiki.tsx
+++ app/components/mizuhiki.tsx
@@ -4,7 +4,7 @@ export default function Mizuhiki() {
 	const redColor = "#E60012";
 
 	return (
-		<div className="w-full h-[100dvh] flex flex-col justify-center items-center overflow-hidden relative">
+		<div className="w-full h-[100svh] flex flex-col justify-center items-center overflow-hidden relative">
 			<div className="opacity-0 animate-fade-in -mt-32 sm:-mt-16 mb-24 sm:mb-0 flex justify-center">
 				<div className="text-4xl sm:text-6xl font-bold [writing-mode:vertical-rl] [text-orientation:upright] font-serif">
 					招待状
